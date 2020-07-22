64ff71ae069cdbdede971cf182cdbdc64ea37b14
U G Murthy <ugmurthy@gmail.com>
2020-07-22 12:34:59 +0530

    Introduced site config variables _data/site.js

---
+++ _data/site.js
@@ -0,0 +1,18 @@
+module.exports = {
+    buildTime: new Date(),
+    blogTitle: "My Blog",
+    author: "U G Murthy",
+    contacts: {
+        email: "ugmurthy@gmail.com",
+        phone: "+99 (080) 8888-8888",
+        address: "Apt 1, Sroff cross, Ave Santa Maria, KA IN 560061",
+        twitter: "@murthyug",
+        twitterUrl: "https://twitter.com/murthyug",
+        githubUrl: "https://github.com/ugmurthy",
+    },
+    showkeywords: 1,
+    showsummary: 0,
+
+
+
+}
